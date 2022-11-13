2f9f20cbe5b8d5def388dfab97648536e740f1ed
a1a2a3a4a5 <linub7@gmail.com>
2022-11-13 11:12:59 +0330

    import & data data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,43 @@
+const { readFileSync } = require('fs');
+const dotenv = require('dotenv');
+const Tour = require('../../models/Tour');
+const connectDB = require('../../config/db');
+
+dotenv.config({
+  path: `./config/config.env`,
+});
+
+// Read JSON File
+const tours = JSON.parse(
+  readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+connectDB();
+
+// Import DATA into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('DATA Successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete All DATA from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('DATA Successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
