9e39241c1d7ad956d718b094281628ab97c9e373
a1a2a3a4a5 <linub7@gmail.com>
2022-11-13 18:07:48 +0330

    refactor filtering sorting limiting fields pagnation

--- controllers/tour.js
+++ controllers/tour.js
@@ -1,67 +1,19 @@
 const Tour = require('../models/Tour');
+const ApiFeature = require('../utils/ApiFeature');
 
 exports.getAllTours = async (req, res) => {
   const { query } = req;
-  // 1) Filtering
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  const queryObj = { ...query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // 2) Advance Filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-
-  let queries = Tour.find(JSON.parse(queryStr));
-
-  // 3) Sorting
-  if (query.sort) {
-    const sortBy = query.sort.split(',').join(' ');
-    queries = queries.sort(sortBy);
-  }
-
-  // 4)  Fields
-  if (query.fields) {
-    const selectedBy = query.fields.split(',').join(' ');
-    queries = queries.select(selectedBy);
-  } else {
-    queries = queries.select('-__v');
-  }
-
-  // 5) Pagination
-  // 1-10 : page1
-  // 11-20 : page2
-  // 21-30 : page3
-  // ...
-  const page = parseInt(query.page, 10) || 1;
-  const limit = parseInt(query.limit, 10) || 100;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Tour.countDocuments();
-
-  queries = queries.skip(startIndex).limit(limit);
-
-  const pagination = {};
-
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
 
   // Execute Query
-  const tours = await queries;
+  const features = new ApiFeature(Tour.find(), query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const tours = await features.query;
   return res.json({
     status: 'success',
-    pagination,
     result: tours.length,
     data: {
       tours,
