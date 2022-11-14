7fae0ee82a6d91a98bc0d0ed5b3897ebf6a1f1a7
a1a2a3a4a5 <linub7@gmail.com>
2022-11-14 12:37:55 +0330

    data validation -> built-in

--- models/Tour.js
+++ models/Tour.js
@@ -10,6 +10,8 @@ const TourSchema = new Schema(
       required: [true, 'Please provide a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal 40 characters'],
+      minlength: [10, 'A tour name must have more or equal 10 characters'],
     },
     slug: String,
     duration: {
@@ -22,12 +24,17 @@ const TourSchema = new Schema(
     },
     difficulty: {
       type: String,
-      enum: ['easy', 'medium', 'difficult'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
       required: [true, 'Please provide difficulty'],
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
