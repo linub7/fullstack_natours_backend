05f2f20faf41113e9a8a279512733a1b9ff448dd
a1a2a3a4a5 <linub7@gmail.com>
2022-11-16 10:16:29 +0330

    validate confirmPassword and password - hashing password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
@@ -31,10 +32,27 @@ const UserSchema = new Schema(
       required: [true, 'Please confirm your password'],
       min: [6, 'password must be at least 6 characters'],
       max: [25, 'password must be less that 26 characters'],
+      validate: {
+        // this only works on .save() or .create()
+        validator: function (val) {
+          return val === this.password;
+        },
+        message: (props) => `${props.value} must be the same password`,
+      },
     },
   },
   // without toJSON: { virtuals: true }, toObject: { virtuals: true } our virtual field will now show
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
+
+UserSchema.pre('save', async function (next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
+});
 
 module.exports = mongoose.model('User', UserSchema);
