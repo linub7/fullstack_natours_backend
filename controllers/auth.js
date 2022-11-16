fa4c663aba30849939fd96195cad62d38d5f1f8d
a1a2a3a4a5 <linub7@gmail.com>
2022-11-16 10:30:07 +0330

    fix security flaw in singup

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,11 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 exports.signup = asyncHandler(async (req, res, next) => {
-  const { body } = req;
+  const {
+    body: { name, email, password, passwordConfirm },
+  } = req;
 
-  const newUser = await User.create(body);
+  const newUser = await User.create({ name, email, password, passwordConfirm });
 
   res.status(201).json({
     status: 'success',
