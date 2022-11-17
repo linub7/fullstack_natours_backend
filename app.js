9890344851c873b9a212d9a4e75fc2fe110c7d40
a1a2a3a4a5 <linub7@gmail.com>
2022-11-17 20:17:33 +0330

    data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/error');
 const errorHandler = require('./middleware/error');
@@ -25,6 +27,12 @@ app.use('/api', limiter);
 
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '2mb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Routes
 readdirSync('./routes').map((route) =>
