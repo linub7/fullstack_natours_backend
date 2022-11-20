7891b6eae7bf6fc449d32c31301fbc44e816fb2e
a1a2a3a4a5 <linub7@gmail.com>
2022-11-20 11:16:23 +0330

    import dummy users / tours / reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,6 +1,8 @@
 const { readFileSync } = require('fs');
 const dotenv = require('dotenv');
 const Tour = require('../../models/Tour');
+const User = require('../../models/User');
+const Review = require('../../models/Review');
 const connectDB = require('../../config/db');
 
 dotenv.config({
@@ -9,13 +11,17 @@ dotenv.config({
 
 // Read JSON File
 const tours = JSON.parse(readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 connectDB();
 
 // Import DATA into DB
 const importData = async () => {
   try {
+    await User.create(users, { validateBeforeSave: false });
     await Tour.create(tours);
+    await Review.create(reviews);
     console.log('DATA Successfully loaded');
   } catch (err) {
     console.log(err);
@@ -27,6 +33,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('DATA Successfully deleted');
   } catch (err) {
     console.log(err);
